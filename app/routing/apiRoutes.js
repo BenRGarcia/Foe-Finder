8a3fd9dd78636d5bda9af2f2874e03f70670454d
Ben Garcia <ben@seebenprogram.com>
2018-04-11 16:47:36 -0400

    require bodyParser for JSON

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,21 @@
 // Require dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
+
+// Mount middleware
+app.use(bodyParser.json());
+
 // Create router
 const apiRouter = express.Router();
 
-// '/api/friends - GET
+// '/api/friends' - GET
 apiRouter.get('/', (req, res, next) => {
-  // This will be used to display a JSON of all possible friends.
+  // display a JSON of all possible friends.
 });
 
 // '/api/friends' - POST
 apiRouter.post('/', (req, res, next) => {
-  // will be used to handle incoming survey results, handle the compatibility logic
+  // handle incoming survey results, compatibility logic
 });
 
 module.exports = apiRouter;
