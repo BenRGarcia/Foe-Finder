4cbd4218f6e2b002f36d308947afe4e69e6cb877
Ben Garcia <ben@seebenprogram.com>
2018-04-12 18:08:48 -0400

    start building Map array method

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,22 +7,49 @@ let foes = require('../data/foes.js');
 const isValid = (newFoe) => {
   const requiredProps = ["name", "quip", "photo", "scores"];
   const foeProps = Object.keys(newFoe);
+  /**
+   *  This input validation is unsophisticated, need to refactor
+   */
           // Correct number of props
   return foeProps.length === requiredProps.length && 
           // All required props present
          foeProps.every(prop => requiredProps.indexOf(prop) !== -1) &&
-          // 'scores' property is correct length
-         newFoe.scores.length === 10;
+          // 'scores' array is correct length
+         newFoe.scores.length === 10 &&
+          // All elements of 'scores' are numbers
+         newFoe.scores.every( score => typeof score === "number");
 };
 
 // Determine arch enemy
-const findArchEnemy = (newFoe) => {
+const archEnemy = (newFoe) => {
+  // Extract answer array from object
   const answers = newFoe.scores;
+  // Declare map to store aggregate results: [ [<index of foe>, <score>], [...], ... ]
+  let foeMap = new Map();
   // Iterate over all foes
-  // perform mathematical sum of abs val of difference of each element between arrays
-  // store in new Map [<score diff>, <index of foe> ]
-  
+  for (let indexOfFoe = 0; indexOfFoe < foes.length; indexOfFoe++) {
+    // Declare counter
+    let cumulativeScore = 0;
+    // Iterate over scores
+    for (let indexOfScore = 0; indexOfScore < answers.length; indexOfScore++) {
+      // perform mathematical sum of abs val of difference of matched element between arrays
+      cumulativeScore += Math.abs( answers[indexOfScore] - foes[indexOfFoe].score[indexOfScore] );
+    }
+    // Add to Map [<index of foe>, <score diff>]
+    foeMap.set(indexOfFoe, cumulativeScore);
+  }
   // Return foe object that has the most compatibility
+  return foes[keyOfHighestValue(foeMap)];
+}
+
+// Returns the 'value' of the mathematically greatest key in a map
+const keyOfHighestValue = (map) => {
+  let highValue;
+  for (true) {
+
+  }
+  // Return Map's value of high key
+  return key;
 }
 
 // Create router
@@ -33,9 +60,8 @@ apiRouter.route('/')
   .get((req, res, next) => res.json(foes))
   .post((req, res, next) => {
     let newFoe = req.body;
-    if (isValid(newFoe)) {
-      
-    }
+    if (isValid(newFoe)) res.send(archEnemy(newFoe))
+    else res.status(400).send();
   });
 
 module.exports = apiRouter;
