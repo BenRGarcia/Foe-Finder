9bfd60248622a1ab268e31106d869a02d558de17
Ben Garcia <ben@seebenprogram.com>
2018-04-12 15:25:55 -0400

    Hard code initial survey results

---
+++ app/data/foes.js
@@ -0,0 +1,28 @@
+let foes = [
+  {
+    "name": "Dr. Evil",
+    "quip": "As long as you don't have 'one millllllion dollars', you'll be fine",
+    "photo": "https://filmracket.com/wp-content/themes/construct/plugins/timthumb.php?src=https://filmracket.com/wp-content/uploads/2013/09/download.jpg&w=660&h=&zc=1",
+    "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },{
+    "name": "Joker",
+    "quip": "Hope you can keep a straight face! No laughing!",
+    "photo": "http://cdn.smosh.com/sites/default/files/2016/03/evil-characters-joker.png",
+    "scores": [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  },{
+    "name": "Hannibal",
+    "quip": "Hope you don't have any fava beans or chianti in the house...",
+    "photo": "http://cdn.smosh.com/sites/default/files/2016/03/evil-characters-hannibal.jpg",
+    "scores": [ 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },{
+    "name": "It",
+    "quip": "Just keep keep an eye out for red balloons. You'll be fine... Probably...",
+    "photo": "http://www.evilclowns.org/wp-content/uploads/2013/10/evil-clown-From-Movies.jpg",
+    "scores": [ 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+  },{
+    "name": "Lord Voldemort",
+    "quip": "For He-Who-Must-Not-Be-Named, there is no safety",
+    "photo": "https://www.ocregister.com/wp-content/uploads/migration/lw6/lw6bws-b78886536z.120111213191646000g7h145pdt.1.jpg?w=620",
+    "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  }
+];
