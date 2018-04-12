495d871dbd72f29c02ab08bee769ce4cfe9a3e7c
Ben Garcia <ben@seebenprogram.com>
2018-04-12 17:01:56 -0400

    refactor route, add input validation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,17 +3,39 @@ const express = require('express');
 const path = require("path");
 let foes = require('../data/foes.js');
 
+// Return true/false if new foe has all required properties
+const isValid = (newFoe) => {
+  const requiredProps = ["name", "quip", "photo", "scores"];
+  const foeProps = Object.keys(newFoe);
+          // Correct number of props
+  return foeProps.length === requiredProps.length && 
+          // All required props present
+         foeProps.every(prop => requiredProps.indexOf(prop) !== -1) &&
+          // 'scores' property is correct length
+         newFoe.scores.length === 10;
+};
+
+// Determine arch enemy
+const findArchEnemy = (newFoe) => {
+  const answers = newFoe.scores;
+  // Iterate over all foes
+  // perform mathematical sum of abs val of difference of each element between arrays
+  // store in new Map [<score diff>, <index of foe> ]
+  
+  // Return foe object that has the most compatibility
+}
+
 // Create router
 const apiRouter = express.Router();
 
-// '/api/friends' - GET
-apiRouter.get('/', (req, res, next) => {
-  res.json(foes);
-});
-
-// '/api/friends' - POST
-apiRouter.post('/', (req, res, next) => {
-  // handle incoming survey results, compatibility logic
-});
+// Path: '/api/friends'
+apiRouter.route('/')
+  .get((req, res, next) => res.json(foes))
+  .post((req, res, next) => {
+    let newFoe = req.body;
+    if (isValid(newFoe)) {
+      
+    }
+  });
 
 module.exports = apiRouter;
